69bda9f346534c1a760b5756cb2617c9ec8f71ec
Ian Ndungu <ianndungu06@gmail.com>
2023-06-14 13:39:44 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(callBack){
+    callBack();
+}
+receivesAFunction();
+
+
+function returnsANamedFunction() {
+    return(function Ordernow(){
+        
+})
+}
+
+function returnsAnAnonymousFunction(){
+    return (function (){
+    
+    })
+}
+    
+
+    
